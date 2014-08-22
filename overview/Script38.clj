47e2eb48239bb35d5f49dcaf4d460014323a5992
AlBaker <albaker@clarkparsia.com>
2014-08-22 00:28:54 -0400

    stardog-clj 0.6

--- overview/Script38.clj
+++ overview/Script38.clj
@@ -1,19 +1,6 @@
 #_(defdeps
     [[org.clojure/clojure "1.6.0"]
-     [stardog-clj "0.5"]
-     [com.complexible.stardog/common-rdf "2.2.1"]
-     [com.complexible.stardog/openrdf-utils "2.2.1"]
-     [com.complexible.stardog/shared "2.2.1"]
-     [org.openrdf.sesame/sesame "2.7.12"]
-     [com.complexible.stardog/api "2.2.1"]
-     [com.complexible.stardog.protocols.snarl/client "2.2.1"]
-     [com.complexible.stardog.protocols.http/client "2.2.1"]
-     [com.complexible.stardog.reasoning.snarl/client "2.2.1"]
-     [com.complexible.stardog.reasoning.http/client "2.2.1"]
-     [com.complexible.stardog.search.snarl/client "2.2.1"]
-     [com.complexible.stardog.search.http/client "2.2.1"]
-     [com.complexible.stardog.icv.snarl/client "2.2.1"]
-     [com.complexible.stardog.icv.http/client "2.2.1"]])
+     [stardog-clj "0.6"]])
 
 ;; uses lein oneoff, e.g. lein oneoff Script21.clj
 
@@ -56,7 +43,7 @@
 ;  (doall (map println (query conn sparql-query ))))
 
 (with-open [c (connect no-reasoning-spec)]
-  (doall (map println (query conn sparql-query ))))
+  (doall (map println (query c sparql-query ))))
 
 
 
--- overview/Script41.clj
+++ overview/Script41.clj
@@ -1,19 +1,6 @@
 #_(defdeps
     [[org.clojure/clojure "1.6.0"]
-     [stardog-clj "0.5"]
-     [com.complexible.stardog/common-rdf "2.2.1"]
-     [com.complexible.stardog/openrdf-utils "2.2.1"]
-     [com.complexible.stardog/shared "2.2.1"]
-     [org.openrdf.sesame/sesame "2.7.12"]
-     [com.complexible.stardog/api "2.2.1"]
-     [com.complexible.stardog.protocols.snarl/client "2.2.1"]
-     [com.complexible.stardog.protocols.http/client "2.2.1"]
-     [com.complexible.stardog.reasoning.snarl/client "2.2.1"]
-     [com.complexible.stardog.reasoning.http/client "2.2.1"]
-     [com.complexible.stardog.search.snarl/client "2.2.1"]
-     [com.complexible.stardog.search.http/client "2.2.1"]
-     [com.complexible.stardog.icv.snarl/client "2.2.1"]
-     [com.complexible.stardog.icv.http/client "2.2.1"]])
+     [stardog-clj "0.6"]])
 
 ;; uses lein oneoff, e.g. lein oneoff Script21.clj
 
